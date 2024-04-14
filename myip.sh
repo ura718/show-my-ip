6a24fe18d1c6ab64444591e5bda254c48d1af072
Yuri Medvinsky <lib-systems@nyu.edu>
2024-04-14 11:52:06 -0400

    refactor: perform sanity check on grep binary

--- myip.sh
+++ myip.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+#
+# Checks your ip and organization assigned it to you
+#
+
+if [ -f /usr/bin/grep ]; then
+    GREP='/usr/bin/grep'
+else 
+    GREP='/bin/grep'
+fi
+
+
+# Get your assigned ip
 myip=$(/usr/bin/curl -s http://checkip.amazonaws.com)
 
-orgname=$(/usr/bin/whois $myip | /usr/bin/grep -E 'Privado|OrgName' | /usr/bin/cut -d: -f2- )
+# Identify which organization assigned you ip
+orgname=$(/usr/bin/whois $myip | $GREP -E 'Privado|OrgName' | /usr/bin/cut -d: -f2- )
 
 printf "$myip $orgname \n"  
